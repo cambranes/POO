99254288cd74315646bf7f8e9f917c37243b5be8
kevinbasto <31493060+kevinbasto@users.noreply.github.com>
2017-09-06 12:42:33 -0500

    changes to my branch
    add bank classes

---
+++ ProjectoFecha/src/projectofecha/Account.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectofecha;
+
+/**
+ *
+ * @author edgar.cambranes
+ */
+/**
+ * @AUTHORS: POO FMAT 
+ */
+public class Account{                           
+    private static int IDkey;
+    private int IDaccount;
+    private double balance; 
+    
+    public Account (double balance) {               
+        this.IDaccount =IDkey;
+        IDkey++;
+        this.balance = balance;
+    }
+    
+    public double withdraw(double ammount){
+        if(balance>=ammount){
+            balance = balance - ammount;
+        }
+        return balance;
+    }
+    
+    public double deposit(double ammount){
+        
+        return balance;
+    }
+    
+    public void setBalance (double balance){
+        this.balance = balance;
+    }
+    
+    public double getBalance (){
+        return balance;
+    }
+}
---
+++ ProjectoFecha/src/projectofecha/Customer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectofecha;
+
+/**
+ *
+ * @author edgar.cambranes
+ */
+public class Customer{
+    private String firstName;
+    private String lastName;
+    private Account account;
+    //cons
+    
+    public Customer (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public Customer (String firstName, String lastName, Account account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.account = account;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void setAccount(Account account){
+        this.account = account;
+    }
+    public Account getAccount(){                
+        return this.account;
+    }
+
+}                               
+
