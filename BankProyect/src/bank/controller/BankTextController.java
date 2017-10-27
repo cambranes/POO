dbe51b45fc4908de4b20a73c571463568a69eb24
Unknown <chikimoco@gmail.com>
2017-10-27 13:03:45 -0500

    Comentarios

--- BankProyect/src/bank/controller/BankTextController.java
+++ BankProyect/src/bank/controller/BankTextController.java
@@ -46,23 +46,29 @@ public class BankTextController {
         return header;
     }
     
-    public String[][] searchHash(String key)
+    //Recibe un hashTable y busca una key, si existe, regresa una tabla con
+    //la informacion del resultado de la busqueda. Si no existe, regresa
+    //un apuntador null
+    public String[][] getHashTable(String key)
     {
-        int j,k;
-        Hashtable<String,String> input = new Hashtable<>();
+        int j;
         BankTextReader bankBuilder = new BankTextReader();
-        input = bankBuilder.bankHashReader("Bank.txt");
-        String[][] output = new String[input.size()][4];
+        Hashtable<String,String> input = bankBuilder.bankHashReader("Bank.txt");
+        String[][] output = new String[0][4];
+        //Si existe el elemento
         if(input.containsKey(key)){
-            
             String line;
             String accounts;
             String[] split;
+            //Recibe la linea del .txt correspondiente
             line = input.get(key);
+            //y lo divide en los elementos necesarios para mostrarlo en una tabla
             split = line.split(",");
+            //colocandolos en la matriz output
             for(j=0;j<3;j++){
                 output[0][j] = split[j];
             }
+            //acomoda todos los IDAccount en un solo elemento de la matriz
             accounts = split[3];
             for(j=6;j<split.length;j++){
                 accounts = accounts + ", " + split[j];
@@ -70,17 +76,9 @@ public class BankTextController {
             }
             output[0][3] = accounts;
         }
+        //Si el elemento no existe, se devuelve null
         else{
-            output[0][0] = "Key";
-            output[0][1] = "Not";
-            output[0][2] = "Found";
-            output[0][3] = "";
-        }
-        for(j=1;j<input.size();j++)
-        {
-            for(k=0;k<4;k++){
-                output[j][k] = "";
-            }
+            output = null;
         }
         return output;
     }
