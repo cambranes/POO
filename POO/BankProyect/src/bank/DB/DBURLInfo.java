49f832c15bcf7ce5f1a259dff41f456dd6759320
Kaher19 <kirbey.garcia19@gmail.com>
2017-11-14 16:26:44 -0600

    Cambios en la lectura de datos para la conexión
    ahora se leen tanto los datos del servidor, como las credenciales del mismo a partir de documentos de texto.
    Se añadieron dos clases que se encargan de lo anterior mencionado.

---
+++ POO/BankProyect/src/bank/DB/DBURLInfo.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bank.DB;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Kirbey
+ */
+public class DBURLInfo {
+    private String dbms;
+    private String serverName;
+    private String serverPort;
+    private String database;
+    private String fullInfo;
+    
+    
+    //Función que recibe el nombre del archivo donde se almacena
+    //la información del URL. Devuelve un string diseñado para la función
+    //getConnection(String, Propperties) en DBConnection.
+    public String getURLInfo(String fileName){
+        try{
+            Scanner scan = new Scanner(new FileReader(fileName));
+            //Uso una coma como delimitador de tokens
+            scan.useDelimiter("\\s*,\\s*");
+            //Mientras hayan tokens en el archivo, los leo
+            while (scan.hasNext()){
+                //Busco el token "dbms" y guardo el valor siguiente en
+                //this.dbms
+                if("dbms".equals(scan.next())){
+                this.dbms= scan.next();
+                }
+                //Busco el token "serverName" y guardo el valor siguiente en
+                //this.serverName
+                if("serverName".equals(scan.next())){
+                    this.serverName = scan.next();
+                }
+                //Busco el token "serverPort" y guardo el valor siguiente en
+                //this.serverPort
+                if("serverPort".equals(scan.next())){
+                    this.serverPort = scan.nextBigInteger().toString();
+                }
+                //Busco el token "database" y guardo el valor siguiente en
+                //this.database
+                if("database".equals(scan.next())){
+                    this.database = scan.next();
+                }
+            }
+            //Asigno a fullinfo el String que devolvera la función
+            //en la forma que se necesita para getConection()
+            fullInfo =  "jdbc:" + this.dbms + "://" +
+                        this.serverName +
+                        ":" + this.serverPort + "/"
+                        + this.database;
+            
+            //Cerramos el scan
+            scan.close();
+        }
+        catch(FileNotFoundException exc)
+           {
+           System.err.print(exc);
+           }
+        //Devolvemos fullinfo como
+        return fullInfo;
+    }
+    
+    //Función que recibe un parámtero String y devuelve un String
+    //correspondiente al dbms
+    public String getDbms(String fileName) throws FileNotFoundException{
+        Scanner scan = new Scanner(new FileReader(fileName));
+        while (scan.hasNext()){
+                if("dbms".equals(scan.next())){
+                this.dbms= scan.next();
+                }
+        }
+        return this.dbms;//<- Pendiente revisar esta parte
+    }
+   public static void main(String args[]){
+        try {
+            DBURLInfo nuevo = new DBURLInfo();
+                    System.out.print("La información de la " +
+                            "conección es "+nuevo.getURLInfo("DBURLInfo.txt"));
+                    System.out.print("\nEl nombre del dbms es " +nuevo.getDbms("DBURLInfo.txt") +"\n");
+        }
+        catch(Exception ex){
+            System.err.print(ex);
+        }
+        
+    }
+}
---
+++ POO/BankProyect/src/bank/controller/BankTextController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank.controller;
+
+import java.util.ArrayList;
+import bank.model.BankTextReader;
+import java.util.Hashtable;
+
+/**
+ *
+ * @author chiki
+ */
+public class BankTextController {
+    
+    
+    public String[][] getTable(String Filename){
+        int i,j;
+        BankTextReader bankBuilder = new BankTextReader();
+        ArrayList<String> input;
+        input = bankBuilder.bankReader(Filename);
+        String[][] output = new String[input.size()][4];
+        String array;
+        String accounts;
+        String[] split;
+        for(i=0;i<input.size();i++){
+            array = input.get(i);
+            split = array.split(",");
+            accounts = split[3];
+            for(j=6;j<split.length;j++){
+                accounts = accounts + ", " + split[j];
+                j = j + 2;
+            }
+            output[i][3] = accounts;
+            for(j=0;j<3;j++){
+                output[i][j] = split[j];
+            }
+        }
+        return output;
+    }
+    
+    public String[] getHeader(){
+        String[] header = {"ID","First Name","Last Name","Accounts"};
+        return header;
+    }
+    
+    //Recibe un hashTable y busca una key, si existe, regresa una tabla con
+    //la informacion del resultado de la busqueda. Si no existe, regresa
+    //un apuntador null
+    public String[][] getHashTable(String key)
+    {
+        int j;
+        BankTextReader bankBuilder = new BankTextReader();
+        Hashtable<String,String> input = bankBuilder.bankHashReader("Bank.txt");
+        String[][] output = new String[0][4];
+        //Si existe el elemento
+        if(input.containsKey(key)){
+            String line;
+            String accounts;
+            String[] split;
+            //Recibe la linea del .txt correspondiente
+            line = input.get(key);
+            //y lo divide en los elementos necesarios para mostrarlo en una tabla
+            split = line.split(",");
+            //colocandolos en la matriz output
+            for(j=0;j<3;j++){
+                output[0][j] = split[j];
+            }
+            //acomoda todos los IDAccount en un solo elemento de la matriz
+            accounts = split[3];
+            for(j=6;j<split.length;j++){
+                accounts = accounts + ", " + split[j];
+                j = j + 2;
+            }
+            output[0][3] = accounts;
+        }
+        //Si el elemento no existe, se devuelve null
+        else{
+            output = null;
+        }
+        return output;
+    }
+}
