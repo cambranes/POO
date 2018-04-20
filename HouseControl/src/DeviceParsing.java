48bf16927d506e22f9887e4ade9fd585fc821dca
Edgar <edgar.cambranes@gmail.com>
2018-04-20 08:49:38 -0500

    Including Devices.txt
    Adding FileReader and DeviceParsing

---
+++ HouseControl/src/DeviceParsing.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author edgar.cambranes
+ */
+public class DeviceParsing {
+    ArrayList<Device> devices;
+    String content;
+    
+    public DeviceParsing(){
+        this.devices = new ArrayList<Device>();
+
+    }
+    
+    public void getString(){
+        content = new FileReader("Devices.txt").getContentFile();
+    
+    }
+    
+    public String[] parsingContent(){
+        
+        String[] parsing= content.split(",");
+        /*for(int i = 0; i<parsing.length; i++){
+            System.out.println(parsing[i]);
+        }*/
+        return parsing;
+    }
+    
+    public void setDevices(String[] parsing){
+        for(int i = 0; i<parsing.length; i=i+3){
+            devices.add(new Device(parsing[i], parsing[i+1], parsing[i+2], true));
+        }
+    }
+    
+    
+    public static void main(String ar[]){
+        DeviceParsing dp = new DeviceParsing();
+        dp.getString();
+        dp.parsingContent();
+    
+    }
+    
+}
---
+++ HouseControl/src/FileReader.java
@@ -0,0 +1,51 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author edgar.cambranes
+ */
+public class FileReader {
+    
+    Scanner file;
+   
+    public FileReader(String path){
+        
+        try {
+            file = new Scanner(new File(path));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+    
+    public String getContentFile(){
+     String content = "";
+        while (file.hasNextLine()) {
+            content+=file.nextLine()+"\n";
+        }
+        file.close();
+    return content;
+    }
+  
+    public void closeFile(){
+        file.close();
+    }
+    
+    
+    public static void main(String ar[]){
+        System.out.println(new FileReader("vices.txt").getContentFile());
+    
+    }
+}
